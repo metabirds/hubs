089b78dc940bb2f6afa3bc90f45cdb09ac4dab6a
metakat <metakatz@gmail.com>
2021-04-22 18:35:00 +0900

    ローカライズ更新

--- src/react-components/ui-root.js
+++ src/react-components/ui-root.js
@@ -1128,7 +1128,7 @@ class UIRoot extends Component {
           },
           {
             id: "preferences",
-            label: "Preferences",
+            label: <FormattedMessage id="more-menu.preferences" defaultMessage="Preferences" />,
             icon: SettingsIcon,
             onClick: () => this.setState({ showPrefs: true })
           }
