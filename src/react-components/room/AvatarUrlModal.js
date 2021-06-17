804c528b10a782396c44472e156a10762f34f035
nshimaya <53924385+nshimaya@users.noreply.github.com>
2021-06-17 20:00:25 +0900

    integrate ReadyPlayerMe

--- src/react-components/room/AvatarUrlModal.js
+++ src/react-components/room/AvatarUrlModal.js
@@ -8,13 +8,26 @@ import { ApplyButton } from "../input/Button";
 import { FormattedMessage } from "react-intl";
 import { Column } from "../layout/Column";
 
+import { ReadyPlayerMeAvatarSelector } from "./ReadyPlayerMeAvatarSelector";
+
 export function AvatarUrlModal({ onSubmit, onClose }) {
-  const { handleSubmit, register } = useForm();
+  const { handleSubmit, register, setValue } = useForm({
+    defaultValues: {
+      url: ""
+    }
+  });
+
+  const handleReadyPlayerMeUrlSubmit = url => {
+    url += "?t=" + Math.floor(Date.now() / 1000);
+    setValue("url", url);
+  };
+
   return (
     <Modal title="Custom Avatar URL" beforeTitle={<CloseButton onClick={onClose} />}>
       <Column as="form" padding center onSubmit={handleSubmit(onSubmit)}>
         <TextInputField
           name="url"
+          id="custom_avatar_url_input"
           label={<FormattedMessage id="avatar-url-modal.avatar-url-label" defaultMessage="Avatar GLB URL" />}
           placeholder="https://example.com/avatar.glb"
           type="url"
@@ -29,6 +42,7 @@ export function AvatarUrlModal({ onSubmit, onClose }) {
             </a>
           }
         />
+        <ReadyPlayerMeAvatarSelector onSubmit={handleReadyPlayerMeUrlSubmit} />
         <ApplyButton type="submit" />
       </Column>
     </Modal>
---
+++ src/react-components/room/ReadyPlayerMeAvatarSelector.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+
+export function ReadyPlayerMeAvatarSelector(props) {
+  const { onSubmit, height, width } = props;
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    function handleEvent(event) {
+      const { data } = event;
+      // readyPlayerMeのメッセージは文字列だけで来る
+      if (typeof (data) === "string" || data instanceof String) {
+        onSubmit(event.data);
+        setOpen(false);
+      }
+    }
+    window.addEventListener("message", handleEvent);
+    return () => {
+      window.removeEventListener('message', handleEvent);
+    };
+  });
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const iframeStyle = {
+    width: width || "90vw",
+    height: height || "80vh",
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    "-webkit-transform": "translate(-50%, -50%)",
+    transform: "translate(-50%, -50%)",
+    border: "1px solid"
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          className="Button__button__2C3ps TextInput__button__1ue_9 Button__primary__emMw- Button__lg__-zn8l"
+          type="button"
+          value="ReadyPlayerMeのURLを取得"
+          onClick={handleOpen}
+          style={{ margin: "0 auto" }}
+        />
+      </div>
+      {
+        open && <div>
+          <iframe
+            title="readyplayerme"
+            id="readyplayerme_iframe"
+            src="https://cyzy.readyplayer.me/avatar"
+            className=""
+            allow="camera *; microphone *"
+            style={iframeStyle}
+          />
+        </div>
+      }
+    </div>
+  );
+}
+
+ReadyPlayerMeAvatarSelector.propTypes = {
+  onSubmit: PropTypes.func
+};
