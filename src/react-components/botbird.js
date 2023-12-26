fc18d8ce2e8b3f00a43ff93b9f956cef203f6dbf
mb-cba <shiiba@metabirds.com>
2023-12-26 12:09:13 +0900

    fix botbird component

--- src/react-components/botbird.js
+++ src/react-components/botbird.js
@@ -1,35 +1,29 @@
 import React, { useState, useEffect } from "react";
 
 const Botbird = () => {
-  // ステートを使用してコンポーネントの表示状態を管理
-  const [enableBotbird, setEnableBotbird] = useState(false);
-  const [chatBoxId, setChatBoxId] = useState("");
+  const [enableCyzyBot, setEnableCyzyBot] = useState(false);
+  const [cyzyBotId, setCyzyBotId] = useState("");
 
   useEffect(() => {
     const handleMessage = e => {
-      // event.data には postMessage で送信されたデータが含まれる
-      const { type, id } = e.data;
+      const { cyzyBot, cyzyBotId } = e.data;
 
-      // メッセージに基づいてコンポーネントの表示状態を更新
-      if (id && type === "on") {
-        setEnableBotbird(true);
-        setChatBoxId(id);
-      } else if (type === "off") {
-        setEnableBotbird(false);
+      if (cyzyBotId && cyzyBot === "enable") {
+        setEnableCyzyBot(true);
+        setCyzyBotId(cyzyBotId);
+      } else if (cyzyBot === "disable") {
+        setEnableCyzyBot(false);
       }
     };
 
-    // イベントリスナーを追加
     window.addEventListener("message", handleMessage);
-
-    // Scriptタグを実行する
     const scriptElement = document.createElement("script");
     scriptElement.src =
-      chatBoxId === "bot1"
+      cyzyBotId === "bot1"
         ? window.cyzyBotId.bot1
-        : chatBoxId === "bot2"
+        : cyzyBotId === "bot2"
         ? window.cyzyBotId.bot2
-        : chatBoxId === "bot3"
+        : cyzyBotId === "bot3"
         ? window.cyzyBotId.bot3
         : "";
     scriptElement.async = true;
@@ -41,9 +35,9 @@ const Botbird = () => {
       window.removeEventListener("message", handleMessage);
       document.body.removeChild(scriptElement);
     };
-  }, [chatBoxId]);
+  }, [cyzyBotId]);
 
-  return enableBotbird ? <div id="pastedChatBox" style={{ position: "absolite", pointerEvents: "all" }} /> : null;
+  return enableCyzyBot ? <div id="pastedChatBox" style={{ position: "absolite", pointerEvents: "all" }} /> : null;
 };
 
 export default Botbird;
